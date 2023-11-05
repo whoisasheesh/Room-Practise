9a52cb5e78284a9cfba36330f0d999039076cf18
Ashish Lamichhane <ashish.lamichhan@bp.com>
2023-11-05 14:43:38 +1100

    Creating contactevent, state and sort type and writing business logic

---
+++ app/src/main/java/com/example/roompractise/ContactViewModel.kt
@@ -0,0 +1,116 @@
+package com.example.roompractise
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class ContactViewModel(
+    private val dao: ContactDao
+) : ViewModel() {
+    private val _sortType = MutableStateFlow(SortType.FIRST_NAME)
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private val _contacts = _sortType
+        .flatMapLatest { sortType ->
+            when (sortType) {
+                SortType.FIRST_NAME -> dao.getContactsSortedByFirstName()
+                SortType.LAST_NAME -> dao.getContactsSortedByLastName()
+                SortType.PHONE_NUMBER -> dao.getContactsSortedByPhoneNumber()
+            }
+        }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
+    private val _state = MutableStateFlow(ContactState())
+    val state = combine(_state, _sortType, _contacts) { state, sortType, contacts ->
+        state.copy(
+            contacts = contacts,
+            sortType = sortType
+        )
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), ContactState())
+
+    fun onEvent(event: ContactEvent) {
+        when (event) {
+            is ContactEvent.DeleteContact -> {
+                viewModelScope.launch {
+                    dao.deleteContact(event.contact)
+                }
+            }
+
+            ContactEvent.HideDialog -> {
+                _state.update {
+                    it.copy(
+                        isAddingContact = false
+                    )
+                }
+            }
+
+            ContactEvent.SaveContact -> {
+                val firstName = state.value.firstName
+                val lastName = state.value.lastName
+                val phoneNumber = state.value.phoneNumber
+
+                if (firstName.isBlank() || lastName.isBlank() || phoneNumber.isBlank()) {
+                    return
+                }
+
+                val contact = Contact(
+                    firstName = firstName,
+                    lastName = lastName,
+                    phoneNumber = phoneNumber
+                )
+                viewModelScope.launch {
+                    dao.upsertContact(contact)
+                }
+                _state.update {
+                    it.copy(
+                        isAddingContact = false,
+                        firstName = "",
+                        lastName = "",
+                        phoneNumber = ""
+                    )
+                }
+            }
+
+            is ContactEvent.SetFirstName -> {
+                _state.update {
+                    it.copy(
+                        firstName = event.firstName
+                    )
+                }
+            }
+
+            is ContactEvent.SetLastName -> {
+                _state.update {
+                    it.copy(
+                        lastName = event.lastName
+                    )
+                }
+            }
+
+            is ContactEvent.SetPhoneNumber -> {
+                _state.update {
+                    it.copy(
+                        phoneNumber = event.phoneNumber
+                    )
+                }
+            }
+
+            ContactEvent.ShowDialog -> {
+                _state.update {
+                    it.copy(
+                        isAddingContact = true
+                    )
+                }
+            }
+
+            is ContactEvent.SortContacts -> {
+                _sortType.value = event.sortType
+            }
+        }
+    }
+}
