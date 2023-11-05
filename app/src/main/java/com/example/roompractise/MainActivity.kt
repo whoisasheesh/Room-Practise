92c642e25dfe2451a6ad268cd02a42557103a0c2
Ashish Lamichhane <ashish.lamichhan@bp.com>
2023-11-05 16:15:02 +1100

    Implementing UI and updating the ksp version to the latest.

--- app/src/main/java/com/example/roompractise/MainActivity.kt
+++ app/src/main/java/com/example/roompractise/MainActivity.kt
@@ -3,14 +3,40 @@ package com.example.roompractise
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
 import com.example.roompractise.ui.theme.RoomPractiseTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val database by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            ContactDatabase::class.java,
+            "contacts.db"
+        ).build()
+    }
+
+    private val viewModel by viewModels<ContactViewModel>(
+        factoryProducer = {
+            object : ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return ContactViewModel(database.dao) as T
+                }
+            }
+        }
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RoomPractiseTheme {
-
+                val state by viewModel.state.collectAsState()
+                ContactScreen(state = state, onEvent = viewModel::onEvent)
             }
         }
     }
